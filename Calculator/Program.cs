83285ab5c532fb4332e6172cb2a0eeae845d150c
Opilane TTHK <opilane@tthk.ee>
2021-10-20 09:50:08 +0300

    first commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta esimene number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta teine number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vali tehe ('+', '/', '-', '*'):");
+            char userOperator = Convert.ToChar(Console.ReadLine());
+
+            switch(userOperator)
+            {
+                case '+':
+                    AddTwoNumbers(num1, num2);
+                    break;
+                case '/':
+                    DivideTwoNumbers(num1, num2);
+                    break;
+                case '-':
+                    SubtractTwoNumber(num1, num2);
+                    break;
+                case '*':
+                    MultiplyTwoNumber(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Vale tehe");
+                    break;
+            }
+
+        }
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine($"{x} + {y} = {result}");
+        }
+        private static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine($"{x} / {y} = {result}");
+        }
+        private static void SubtractTwoNumber(double x, double y)
+        {
+            double result = x - y;
+            Console.WriteLine($"{x} - {y} = {result}");
+        }
+        private static void MultiplyTwoNumber(double x, double y)
+        {
+            double result = x * y;
+            Console.WriteLine($"{x} * {y} = {result}");
+        }
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma eesnimi");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta sünniaasta.");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta ome perekonnanime:");
+            string userLastname = Console.ReadLine();
+
+
+            Greetings(userName, userLastname, userAge);
+
+
+        }
+        private static void Greetings(string someString, string anotherString, int someInt)
+        {
+            Console.WriteLine($"Tere, {someString} {anotherString}!");
+            Console.WriteLine($"Oled {2021 - someInt}.");
+        }
+    }
+}
